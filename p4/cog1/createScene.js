595d2021d7a6e110d4aed43fa6d615dc20277fdb
Jeffrey King <jk95@hotmail.de>
2022-04-14 16:02:32 +0200

    Cude setVisible false

--- p4/cog1/createScene.js
+++ p4/cog1/createScene.js
@@ -18,8 +18,9 @@ function(exports, scenegraph, animation) {
 	function init() {
 
 		var insideOutPolyNode = scenegraph.createNodeWithModel("insideOutPoly", "insideOutPoly");
-		 var cubeNode = scenegraph.createNodeWithModel("cube", "cube", {scale:300});
-		 cubeNode.rotateTo([1.6, -3.87, 0]);
+		var cubeNode = scenegraph.createNodeWithModel("cube", "cube", {scale:300});
+		cubeNode.rotateTo([1.6, -3.87, 0]);
+		cubeNode.setVisible(false);
 
 		// BEGIN exercise myModel
 		
